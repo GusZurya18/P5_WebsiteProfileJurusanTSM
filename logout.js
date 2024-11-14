aa56a660bbdfb60256428d85eeae8da0e7ff225e
idabagussuryadharma218 <idabagussuryadharma218@gmail.com>
2024-11-14 15:33:48 +0000

    initial commit

--- logout.js
+++ logout.js
@@ -1,24 +1,24 @@
 function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
-  function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
+  return "";
+}
 
 // Get the modal
 var modal = document.getElementById("logout-modal");
@@ -33,35 +33,38 @@ var cancelLogout = document.getElementById("cancel-logout");
 
 // When the user clicks on the logout link, open the modal
 logoutLink.onclick = function (event) {
-    setCookie("isLogin", "", 7);
-    event.preventDefault(); // Prevent the default action (navigation)
-    modal.style.display = "block"; // Show the modal
+  setCookie("isLogin", "", 7);
+  event.preventDefault(); // Prevent the default action (navigation)
+  modal.style.display = "block"; // Show the modal
 }
 
 // When the user clicks on the cancel button, close the modal
 cancelLogout.onclick = function () {
-    modal.style.display = "none"; // Hide the modal
+  modal.style.display = "none"; // Hide the modal
 }
 
 // When the user clicks on the confirm button, redirect to the logout URL
 confirmLogout.onclick = function () {
-    window.location.href = "index.html"; // Replace with your actual logout URL
+  window.location.href = "index.html"; // Replace with your actual logout URL
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none"; // Hide the modal
-    }
+  if (event.target == modal) {
+      modal.style.display = "none"; // Hide the modal
+  }
 }
 
 function changeLogin(){
-    setCookie("isLogin", "true", 7);
+  setCookie("isLogin", "true", 7);
 }
 
 document.addEventListener("DOMContentLoaded", function(e) {
-    var url = window.location.pathname
-    if(!url.includes("login") && !url.includes("register") && getCookie("isLogin") == "") return window.location.replace("/login")
+  var url = window.location.pathname
+  if(!url.includes("login") && !url.includes("register") && getCookie("isLogin") !=="true") 
+  {
+    window.location.replace("/index.html");
+  }
 });
 
 
